82d04a67814f7668ce9b41e29961475deee4bc80
Hannes Biribauer <mail@hannesbiribauer.com>
2016-09-10 18:50:00 +0200

    initial commit

---
+++ Assets/Scripts/GoToMainTower.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GoToMainTower : MonoBehaviour {
+
+	public float distanceToBlowUp = 0.1F;
+
+	// Use this for initialization
+	void Start () {
+		GetComponent<AIPath>().target = GameObject.FindGameObjectWithTag ("MainTower").transform;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (Vector3.Distance (GetComponent<AIPath> ().target.position, this.transform.position) < distanceToBlowUp) {
+			//GameObject.FindGameObjectWithTag ("MainTower")
+			DestroyImmediate(this.gameObject);
+		}
+	}
+}
---
+++ Assets/Scripts/Spawner.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Spawner : MonoBehaviour {
+
+	public GameObject ennemyUnit;
+	public int intervalSeconds;
+	private float time;
+
+	// Use this for initialization
+	void Start () {
+		time = intervalSeconds;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		time += Time.deltaTime;
+
+		if (time < intervalSeconds)
+			return;
+
+		time = 0;
+
+		GameObject go = GameObject.Instantiate (ennemyUnit);
+		go.transform.position = this.transform.position;
+		go.transform.parent = this.transform;
+	}
+}

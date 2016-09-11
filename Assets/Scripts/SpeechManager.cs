4d24bf9ae7e5ff8db804a56c476a56dd42472f03
Hannes Biribauer <mail@hannesbiribauer.com>
2016-09-11 13:25:11 +0200

    updated terrain and health system

--- Assets/Scripts/SpeechManager.cs
+++ Assets/Scripts/SpeechManager.cs
@@ -1,6 +1,7 @@
 ﻿using System.Collections.Generic;
 using System.Linq;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 using UnityEngine.Windows.Speech;
 
 public class SpeechManager : MonoBehaviour
@@ -8,70 +9,34 @@ public class SpeechManager : MonoBehaviour
     KeywordRecognizer keywordRecognizer = null;
     Dictionary<string, System.Action> keywords = new Dictionary<string, System.Action>();
 
-    public Transform tower;
+    public Transform tower;  // inspector object to hold our tower
 
     // Use this for initialization
     void Start()
     {
-        keywords.Add("Reset world", () =>
+        keywords.Add("Restart game", () =>
         {
             // Call the OnReset method on every descendant object.
-            this.BroadcastMessage("OnReset");
+            SceneManager.LoadScene(0);
+            //this.BroadcastMessage("OnReset");
         });
 
-        //keywords.Add("Drop Sphere", () =>
-        //{
-        //    var focusObject = GazeGestureManager.Instance.FocusedObject;
-        //    if (focusObject != null)
-        //    {
-        //        // Call the OnDrop method on just the focused object.
-        //        focusObject.SendMessage("OnDrop");
-        //    }
-        //});
-
-        keywords.Add("Place Tower", () =>
+        keywords.Add("Pause game", () =>
         {
-            // Do a raycast into the world based on the user's
-            // head position and orientation.
-            var headPosition = Camera.main.transform.position;
-            var gazeDirection = Camera.main.transform.forward;
-
-            RaycastHit hitInfo;
-
-            if (Physics.Raycast(headPosition, gazeDirection, out hitInfo))
-            {
-                //Quaternion toQuat = Camera.main.transform.localRotation;
-
-                // If the raycast hit a hologram...
-                // Display the cursor mesh.
-                var towerToPlace = Object.Instantiate(tower);
-                towerToPlace.transform.position = hitInfo.point;
-                //towerToPlace.transform.rotation = toQuat;
-            }
+            // Call the OnReset method on every descendant object.
+            Time.timeScale = 0.01f;
         });
 
-
-        keywords.Add("Destroy Tower", () =>
+        keywords.Add("Resume game", () =>
         {
-            // Do a raycast into the world based on the user's
-            // head position and orientation.
-            var headPosition = Camera.main.transform.position;
-            var gazeDirection = Camera.main.transform.forward;
-
-            RaycastHit hitInfo;
-
-            if (Physics.Raycast(headPosition, gazeDirection, out hitInfo))
-            {
-                if (hitInfo.transform.tag == "Tower")
-                {
-                    var exploder = hitInfo.transform.gameObject.GetComponent<MeshExploder>();
-                    {
-                        exploder.Explode();
-                        GameObject.Destroy(hitInfo.transform.gameObject);
-                    }
-                }
-            }
+            // Call the OnReset method on every descendant object.
+            //this.BroadcastMessage("OnReset");
+            Time.timeScale = 1.0f;
         });
+
+        keywords.Add("Tower", PlaceTower);
+
+        keywords.Add("Destroy", DestroyTower);
 
         // Tell the KeywordRecognizer about our keywords.
         keywordRecognizer = new KeywordRecognizer(keywords.Keys.ToArray());
@@ -79,6 +44,49 @@ public class SpeechManager : MonoBehaviour
         // Register a callback for the KeywordRecognizer and start recognizing!
         keywordRecognizer.OnPhraseRecognized += KeywordRecognizer_OnPhraseRecognized;
         keywordRecognizer.Start();
+    }
+
+    private void PlaceTower()
+    {
+        // Do a raycast into the world based on the user's
+        // head position and orientation.
+        var headPosition = Camera.main.transform.position;
+        var gazeDirection = Camera.main.transform.forward;
+
+        RaycastHit hitInfo;
+
+        if (Physics.Raycast(headPosition, gazeDirection, out hitInfo))
+        {
+            //Quaternion toQuat = Camera.main.transform.localRotation;
+
+            // If the raycast hit a hologram...
+            // Display the cursor mesh.
+            var towerToPlace = Object.Instantiate(tower);
+            towerToPlace.transform.position = hitInfo.point;
+            //towerToPlace.transform.rotation = toQuat;
+        }
+    }
+
+    private void DestroyTower()
+    {
+        // Do a raycast into the world based on the user's
+        // head position and orientation.
+        var headPosition = Camera.main.transform.position;
+        var gazeDirection = Camera.main.transform.forward;
+
+        RaycastHit hitInfo;
+
+        if (Physics.Raycast(headPosition, gazeDirection, out hitInfo))
+        {
+            if (hitInfo.transform.tag == "Tower")
+            {
+                var exploder = hitInfo.transform.gameObject.GetComponent<MeshExploder>();
+                {
+                    exploder.Explode();
+                    GameObject.Destroy(hitInfo.transform.gameObject);
+                }
+            }
+        }
     }
 
     private void KeywordRecognizer_OnPhraseRecognized(PhraseRecognizedEventArgs args)

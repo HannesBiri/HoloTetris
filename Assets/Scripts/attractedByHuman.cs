059dcc6677d8a0f4e17af38bd0cd1e399efe8de2
Hannes Biribauer <mail@hannesbiribauer.com>
2016-09-23 10:00:34 +0200

    project cleanup and renamed to HoloDefense

---
+++ Assets/Scripts/attractedByHuman.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class attractedByHuman : MonoBehaviour
+{
+	private Transform human;
+	public float force =  30;
+	// Use this for initialization
+	void Start ()
+	{
+		human = FindObjectOfType<Camera>().gameObject.transform;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		Vector3 direction = (human.position - transform.position);
+		direction.Normalize() ;
+
+         GetComponent<Rigidbody>().AddForce(direction * force * Time.deltaTime);
+
+	}
+}

f23067c928b3956346a021c5bac1a474c42f1916
Hannes Biribauer <mail@hannesbiribauer.com>
2016-09-23 14:24:15 +0200

    camera positioning fixes

--- Assets/Scripts/CameraPositioner.cs
+++ Assets/Scripts/CameraPositioner.cs
@@ -10,14 +10,15 @@ public class CameraPositioner : MonoBehaviour {
         if (VRDevice.isPresent)
         {
             // when running on hololens, we ARE the camera and we are always on position 0, 0, 0
-            this.transform.position = new Vector3(0, 0, 0);
-            Camera.main.rect = new Rect(0, 0, 0.5f, 0.5f);
+            Camera.main.transform.position = new Vector3(0, 0, 0);
+            Camera.main.transform.localEulerAngles = new Vector3(0.0f, 0.0f, 0.0f);
+            Camera.main.rect = new Rect(0.0f, 0.0f, 0.5f, 0.5f);
         }
         else
         {
-            this.transform.position = new Vector3(0.3f, 1.0f, 1.4f);
-            this.transform.localEulerAngles = new Vector3(70.0f, -30.0f, -30.0f); // .rotation = new Quaternion(0.2f, 0.0f, 0.0f, 0.0f);
-            Camera.main.rect = new Rect(0, 0, 1, 1);
+            Camera.main.transform.position = new Vector3(0.3f, 1.0f, 1.4f);
+            Camera.main.transform.localEulerAngles = new Vector3(70.0f, -30.0f, -30.0f);
+            Camera.main.rect = new Rect(0.0f, 0.0f, 1.0f, 1.0f);
         }
     }
 }
